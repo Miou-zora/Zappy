577cf6fed13ba2b75a8c15109833fad1e4b5f80b
Jibea <87119218+Jibea@users.noreply.github.com>
2023-06-05 16:18:11 +0200

    [Game Dev] : add the objects struct (#70)
    Co-authored-by: Mathis <63660263+Kibatsu03@users.noreply.github.com>

---
+++ server/include/all_objects.h
@@ -0,0 +1,30 @@
+/*
+** EPITECH PROJECT, 2023
+** Zappy-Mirror
+** File description:
+** all_objects
+*/
+
+#pragma once
+
+
+/**
+ * @brief Create a enum for all objects
+ */
+enum {
+    FOOD,
+    LINEMATE,
+    DERAUMERE,
+    SIBUR,
+    MENDIANE,
+    PHIRAS,
+    THYSTAME
+};
+
+
+/**
+ * @brief Create a struct for all objects
+ */
+typedef struct object_s {
+    int nb_of_objects[7];
+} object_t;
