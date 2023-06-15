01607a5b12757f4e89f9bd3f325230476b3d96a5
Miouzora <91665686+Miou-zora@users.noreply.github.com>
2023-06-15 10:06:39 +0200

    [GUI .obj]: load obj file into model (#195)
    Co-authored-by: quentin <Quentin.brejoin@gmail.com>

--- GUI/src/utils/ObjLoader.hpp
+++ GUI/src/utils/ObjLoader.hpp
@@ -14,6 +14,7 @@
 #include <sstream>
 #include <cstring>
 #include "ErrorManagement.hpp"
+#include "Model.hpp"
 #include <map>
 
 namespace GUI::Utils {
@@ -34,7 +35,6 @@ namespace GUI::Utils {
                 std::vector<glm::vec3>& normals)
             {
                 bool thingsToRead = true;
-                // ptr on function
                 std::map<std::string, void (*)(
                     std::ifstream &,
                     std::vector<unsigned int>&,
@@ -48,15 +48,16 @@ namespace GUI::Utils {
                     {"vn", &readObjFileNormal},
                     {"f", &readObjFileFace}
                 };
+                std::string topHeader = "";
 
                 while (thingsToRead) {
-                    std::string TopHeader;
-                    file >> TopHeader;
+                    topHeader = "";
+                    file >> topHeader;
                     if (file.eof())
                         thingsToRead = false;
                     else {
-                        if (ptrOnFunction.find(TopHeader) != ptrOnFunction.end()) {
-                            ptrOnFunction[TopHeader](file, vertexIndices, uvIndices, normalIndices, vertices, uvs, normals);
+                        if (ptrOnFunction.find(topHeader) != ptrOnFunction.end()) {
+                            ptrOnFunction[topHeader](file, vertexIndices, uvIndices, normalIndices, vertices, uvs, normals);
                         }
                     }
                 }
@@ -75,6 +76,47 @@ namespace GUI::Utils {
                     throw GUI::ObjLoadException("Error: Cannot open file " + filePath);
                 }
                 return file;
+            }
+
+            /**
+             * @brief Load the .obj file and return a model. It load the mesh.
+             *
+             * @return std::shared_ptr<GUI::Graphic::Object::Model> model
+             */
+            static std::shared_ptr<GUI::Graphic::Object::Model> createModelFromObjFilePath(const std::string &path)
+            {
+                std::ifstream file = openFile(path);
+                std::vector<unsigned int> vertexIndices, uvIndices, normalIndices;
+                std::vector<glm::vec3> vertices;
+                std::vector<glm::vec2> uvs;
+                std::vector<glm::vec3> normals;
+                std::shared_ptr<GUI::Graphic::Object::Mesh> mesh = std::make_shared<GUI::Graphic::Object::Mesh>();
+                std::shared_ptr<GUI::Graphic::Object::Model> model = std::make_shared<GUI::Graphic::Object::Model>();
+
+                readObjFile(file, vertexIndices, uvIndices, normalIndices, vertices, uvs, normals);
+                for (unsigned int i = 0; i < vertexIndices.size(); i++) {
+                    mesh->addVertex(vertices[vertexIndices[i] - 1].x, vertices[vertexIndices[i] - 1].y, vertices[vertexIndices[i] - 1].z);
+                }
+                for (unsigned int i = 0; i < uvIndices.size(); i++) {
+                    mesh->addUV(uvs[uvIndices[i] - 1].x, uvs[uvIndices[i] - 1].y);
+                }
+                if (normalIndices.size() != 0)
+                    for (unsigned int i = 0; i < normalIndices.size(); i++)
+                        mesh->addNormal(normals[normalIndices[i] - 1].x, normals[normalIndices[i] - 1].y, normals[normalIndices[i] - 1].z);
+                else {
+                    for (unsigned int i = 0; i < vertexIndices.size() / 3; i++) {
+                        glm::vec3 v1 = vertices[vertexIndices[i * 3] - 1];
+                        glm::vec3 v2 = vertices[vertexIndices[i * 3 + 1] - 1];
+                        glm::vec3 v3 = vertices[vertexIndices[i * 3 + 2] - 1];
+                        glm::vec3 normal = glm::normalize(glm::cross(v2 - v1, v3 - v1));
+                        mesh->addNormal(normal.x, normal.y, normal.z);
+                        mesh->addNormal(normal.x, normal.y, normal.z);
+                        mesh->addNormal(normal.x, normal.y, normal.z);
+                    }
+                }
+                mesh->load();
+                model->addMesh(mesh);
+                return model;
             }
 
         private:
@@ -140,28 +182,41 @@ namespace GUI::Utils {
                 std::vector<glm::vec2>& uvs,
                 std::vector<glm::vec3>& normals)
             {
+                (void)normalIndices;
                 (void)vertices;
                 (void)uvs;
                 (void)normals;
-                std::string vertex1, vertex2, vertex3;
-                unsigned int vertexIndex[3], uvIndex[3], normalIndex[3];
-                char slash;
-                file >> vertexIndex[0] >> slash >> uvIndex[0] >> slash >> normalIndex[0]
-                >> vertexIndex[1] >> slash >> uvIndex[1] >> slash >> normalIndex[1]
-                >> vertexIndex[2] >> slash >> uvIndex[2] >> slash >> normalIndex[2];
+                unsigned int vertexIndex[4], uvIndex[4];
+                char slash = 0;
+                size_t cursor = 0;
 
-                if (slash != '/') {
-                    throw GUI::ObjLoadException("Error: Cannot read file");
+                while (file.peek() != '\n' && file.peek() != '\r' && file.peek() != EOF) {
+                    file >> vertexIndex[cursor] >> slash >> uvIndex[cursor];
+                    file.get();
+                    cursor++;
                 }
-                vertexIndices.push_back(vertexIndex[0]);
-                vertexIndices.push_back(vertexIndex[1]);
-                vertexIndices.push_back(vertexIndex[2]);
-                uvIndices.push_back(uvIndex[0]);
-                uvIndices.push_back(uvIndex[1]);
-                uvIndices.push_back(uvIndex[2]);
-                normalIndices.push_back(normalIndex[0]);
-                normalIndices.push_back(normalIndex[1]);
-                normalIndices.push_back(normalIndex[2]);
+
+                if (cursor == 3) {
+                    vertexIndices.push_back(vertexIndex[0]);
+                    uvIndices.push_back(uvIndex[0]);
+                    vertexIndices.push_back(vertexIndex[1]);
+                    uvIndices.push_back(uvIndex[1]);
+                    vertexIndices.push_back(vertexIndex[2]);
+                    uvIndices.push_back(uvIndex[2]);
+                } else if (cursor == 4) {
+                    vertexIndices.push_back(vertexIndex[0]);
+                    uvIndices.push_back(uvIndex[0]);
+                    vertexIndices.push_back(vertexIndex[1]);
+                    uvIndices.push_back(uvIndex[1]);
+                    vertexIndices.push_back(vertexIndex[2]);
+                    uvIndices.push_back(uvIndex[2]);
+                    vertexIndices.push_back(vertexIndex[2]);
+                    uvIndices.push_back(uvIndex[2]);
+                    vertexIndices.push_back(vertexIndex[3]);
+                    uvIndices.push_back(uvIndex[3]);
+                    vertexIndices.push_back(vertexIndex[0]);
+                    uvIndices.push_back(uvIndex[0]);
+                }
             }
     };
 }
---
+++ GUI/tests/ftObjLoader/ftObjLoader.cpp
@@ -0,0 +1,40 @@
+/*
+** EPITECH PROJECT, 2023
+** Zappy-Mirror
+** File description:
+** ftObjLoader
+*/
+
+#include "Cube.hpp"
+#include "Management.hpp"
+#include "ObjLoader.hpp"
+#include <iostream>
+
+int main(int ac, char **av)
+{
+    (void)ac;
+    (void)av;
+    GUI::Graphic::Management management;
+    std::shared_ptr<GUI::Graphic::Object::Model> model;
+    std::shared_ptr<GUI::Graphic::Program> program = std::make_shared<GUI::Graphic::Program>();
+
+    management.init();
+    management.getScene()->getCamera()->setPos(glm::vec3(0.0f, -70.0f, -50.0f));
+    program->load("tests/ftObjLoader/vertexShader.vertexshader", "tests/ftObjLoader/fragmentShader.fragmentshader");
+
+    std::cout << "Loading model..." << std::endl;
+    model = GUI::Utils::ObjLoader::createModelFromObjFilePath("tests/ftObjLoader/test.obj");
+    std::cout << "Model loaded" << std::endl;
+    model->setRot(glm::vec3(0.0f, 0.0f, M_PI));
+    model->setProgram(program);
+    model->getMesh(0)->loadProgram(program);
+    model->setProgram(program);
+    management.getScene()->addModel(model);
+    while (management.isRunning()) {
+        management.pollEvent();
+        management.update();
+        management.render();
+    }
+
+    management.close();
+}
